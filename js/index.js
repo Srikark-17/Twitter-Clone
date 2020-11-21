5e0a51f41cdf5bf0a189994a0273bb040128a211
Srikark-17 <srikar.sgk@gmail.com>
2020-11-20 18:44:27 -0600

    Show tweets based on API data

--- js/index.js
+++ js/index.js
@@ -20,7 +20,7 @@ const getTwitterData = () => {
       return response.json();
     })
     .then((data) => {
-      console.log(data);
+      buildTweets(data.statuses);
     })
     .catch((err) => console.log(err));
 };
@@ -43,7 +43,35 @@ const nextPageButtonVisibility = (metadata) => {};
 /**
  * Build Tweets HTML based on Data from API
  */
-const buildTweets = (tweets, nextPage) => {};
+const buildTweets = (tweets, nextPage) => {
+  let twitterContent = "";
+  tweets.map((tweet) => {
+    twitterContent += `
+    <div class="tweet-user-info">
+        <div class="tweet-user-profile">
+        </div>
+        <div class="tweet-user-name-container">
+            <div class="tweet-user-fullname">
+                Lorem ipsum
+            </div>
+            <div class="tweet-user-username">
+                @realloremipsum
+            </div>
+        </div>
+    </div>
+    <div class="tweet-images-container">
+        <div class="tweet-image"></div>
+    </div>
+    <div class="tweet-text-container">
+        ${tweet.full_text}
+    </div>
+    <div class="tweet-date-container">
+        20 hours ago
+    </div>`;
+  });
+
+  document.querySelector(".tweets-list").innerHTML = twitterContent;
+};
 
 /**
  * Build HTML for Tweets Images
